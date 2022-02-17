5842305d4b1ff01a73d05e91fdec9bc2cdab7a9a
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-02-17 20:58:22 +0000

    commit

--- counsellingApp/urls.py
+++ counsellingApp/urls.py
@@ -6,12 +6,25 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 app_name = 'counsellingUrls'
 
 urlpatterns = [
-    path('home/<str:pk>/', views.homePage, name="homePage"),
     path('', views.loginPage, name="loginPage"),
-    path('ad/', views.adminPage, name="adminPage"),
-    path('profile/', views.profilePage, name="profilePage"),
-    path('search/', views.search, name="search"),
-    path('delete/<str:pk>/', views.delete, name="delete"),
+    path('search/<str:pk>/', views.search, name="search"),
+    path('dashboard/<str:pk>/', views.adminPage, name="adminPage"),
+    path('admindashboard/<str:pk>/', views.superadminPage, name="superadminPage"),
+    path('admindashboard/<str:pk>/', views.superadminPage, name="superadminPage"),
+    path('admindashboard/<str:pk>/', views.superadminPage, name="superadminPage"),
+    path('admindashboard/<str:pk>/', views.superadminPage, name="superadminPage"),
+
+    path('home/<str:pk>/', views.homePage, name="homePage"),
+    path('notifications/<str:pk>/',
+         views.notificationPage, name="notificationPage"),
+    path('index/<str:pk>/',
+         views.indexPage, name="indexPage"),
+
+    path('profile/<str:pk>/', views.profilePage, name="profilePage"),
+    path('delete/<str:studentbook_id>/', views.delete, name="delete"),
+    path('update/<str:studentbook_id>/<str:studentbook_status>',
+         views.update, name="update"),
+
 
 ]
 
