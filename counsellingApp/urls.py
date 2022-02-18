811a5d2a5633becc406b6e0a647f3b4326da8cab
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-02-18 01:00:52 +0000

    commit

--- counsellingApp/urls.py
+++ counsellingApp/urls.py
@@ -8,18 +8,15 @@ app_name = 'counsellingUrls'
 urlpatterns = [
     path('', views.loginPage, name="loginPage"),
     path('search/<str:pk>/', views.search, name="search"),
+    path('admindashboard/<str:pk>/', views.superadminPage, name="superadminPage"),
     path('dashboard/<str:pk>/', views.adminPage, name="adminPage"),
-    path('admindashboard/<str:pk>/', views.superadminPage, name="superadminPage"),
-    path('admindashboard/<str:pk>/', views.superadminPage, name="superadminPage"),
-    path('admindashboard/<str:pk>/', views.superadminPage, name="superadminPage"),
-    path('admindashboard/<str:pk>/', views.superadminPage, name="superadminPage"),
-
+    path('edit/<str:pk>/', views.dashboardPage, name="dashboardPage"),
+    path('settings/<str:pk>/', views.settingsPage, name="settingsPage"),
     path('home/<str:pk>/', views.homePage, name="homePage"),
     path('notifications/<str:pk>/',
          views.notificationPage, name="notificationPage"),
     path('index/<str:pk>/',
          views.indexPage, name="indexPage"),
-
     path('profile/<str:pk>/', views.profilePage, name="profilePage"),
     path('delete/<str:studentbook_id>/', views.delete, name="delete"),
     path('update/<str:studentbook_id>/<str:studentbook_status>',
