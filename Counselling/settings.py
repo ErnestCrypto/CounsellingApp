d6bc8e35e67ef7d2c1458fdf1a3b0e7e25081236
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-03-22 17:58:34 +0000

    commit

--- Counselling/settings.py
+++ Counselling/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import environ
-env = environ.Env()
-environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-21#@wwn+1ti3y=y4ys-w2hb4b$j_=+0l$wmn=c99%$--@6a4lr'
-SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -87,9 +84,7 @@ DATABASES = {
 
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASS'),
+
 
 
 
