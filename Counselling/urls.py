e83164721518ede4cf7dd6ddf8cc75a85869b35d
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-03-25 02:16:54 +0000

    commit]

--- Counselling/urls.py
+++ Counselling/urls.py
@@ -1,12 +1,12 @@
 
+from ast import pattern
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('counsellingApp.urls', namespace='counsellingUrls')),
@@ -19,7 +19,7 @@ if settings.DEBUG:
                           document_root=settings.MEDIA_ROOT)
 
 
-urlpatterns += path('',
-                    (r'^%s/' % settings.DAJAXICE_MEDIA_PREFIX, include('dajaxice.urls')),)
+urlpatterns += pattern('',
+                       re_path(r'^%s/' % settings.DAJAXICE_MEDIA_PREFIX, include('dajaxice.urls')),)
 
 urlpatterns += staticfiles_urlpatterns()
