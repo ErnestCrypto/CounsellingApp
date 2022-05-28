5a2a8046ebfad59be3d6d75e50c2329fa9c08061
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-05-28 17:33:06 +0000

    commit

---
+++ counsellingApp/templates/app/assets/js/index.js
@@ -0,0 +1,15 @@
+
+<script>
+  JSC.Chart('chartDiv', {
+   type: 'horizontal column',
+   series: [
+      {
+         points: [
+            {x: 'Apples', y: 50},
+            {x: 'Oranges', y: 42}
+         ]
+      }
+   ]
+});
+
+</script>
