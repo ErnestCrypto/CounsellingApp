2b989c84feb8a95a4bba90a52dd1b928df2889fa
ErnestCrypto <akotobamfo.eab@gmail.com>
2022-06-13 13:49:50 +0000

    commit

--- bookingApp/views.py
+++ bookingApp/views.py
@@ -9,45 +9,56 @@ from django.conf import settings
 def index(request, pk):
     request.session['pk'] = pk
     students = Students.objects.all()
+    counsellors = Counsellor.objects.all()
     return render(request, 'index.html', {
         'pk': pk,
         'students': students,
+        'counsellors': counsellors,
     })
 
 
 def about(request):
     pk = request.session['pk']
     students = Students.objects.all()
+    counsellors = Counsellor.objects.all()
     return render(request, 'about.html', {
         'pk': pk,
         'students': students,
+        'counsellors': counsellors,
     })
 
 
 def blog_single(request):
     pk = request.session['pk']
     students = Students.objects.all()
+    counsellors = Counsellor.objects.all()
     return render(request, 'blog-single.html', {
         'pk': pk,
         'students': students,
+        'counsellors': counsellors,
     })
 
 
 def blog(request):
     pk = request.session['pk']
     students = Students.objects.all()
+    counsellors = Counsellor.objects.all()
     return render(request, 'blog.html', {
         'pk': pk,
         'students': students,
+        'counsellors': counsellors,
+
     })
 
 
 def contact(request):
     pk = request.session['pk']
     students = Students.objects.all()
+    counsellors = Counsellor.objects.all()
     return render(request, 'contact.html', {
         'pk': pk,
         'students': students,
+        'counsellors': counsellors,
     })
 
 
@@ -55,10 +66,12 @@ def counselor(request):
     objects = Counsellor.objects.all()
     pk = request.session['pk']
     students = Students.objects.all()
+    counsellors = Counsellor.objects.all()
     return render(request, 'counselor.html', {
         'objects': objects,
         'pk': pk,
         'students': students,
+        'counsellors': counsellors,
     })
 
 
@@ -69,21 +82,25 @@ def details(request, object_id):
     students = Students.objects.all()
     coun = Counsellor.objects.get(user_id=object_id)
     availables = Availability.objects.filter(counsellor=coun)
+    counsellors = Counsellor.objects.all()
     return render(request, 'details.html', {
         'objects': objects,
         'counselor': counselor,
         'pk': pk,
         'students': students,
         'availables': availables,
+        'counsellors': counsellors,
     })
 
 
 def services(request):
     pk = request.session['pk']
     students = Students.objects.all()
+    counsellors = Counsellor.objects.all()
     return render(request, 'services.html', {
         'pk': pk,
         'students': students,
+        'counsellors': counsellors,
     })
 
 
@@ -138,7 +155,8 @@ def book(request, pk, object_user_id):
             book.counsellor_user_id = counsellor.user_id
             book.student_id = student.student_id
             book.student_name = stud_name
+            book.save()
 
     return render(request, 'book.html', {
-        'pk': pk
+        'pk': pk,
     })
